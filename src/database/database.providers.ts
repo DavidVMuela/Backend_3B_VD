6b0ef6566d0cf0f024ef06922563e2c87c6ceefb
DavidVMuela <dom.villagomez@yavirac.edu.ec>
2024-11-07 21:54:21 -0500

    proyecto base

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -1,10 +1,13 @@
+import { Inject } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
 import { DataSource } from "typeorm"
 
 
 export const databaseProvider=[
     {
         provide: 'DATABASE_CNNECTION',
-        useFactory:()=>{
+        inject: [ConfigService],
+        useFactory:(config: ConfigService)=>{
             const dataSource= new DataSource({
                 type:'postgres',
                 host:'localhost',
